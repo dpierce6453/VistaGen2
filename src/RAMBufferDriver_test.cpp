2cac1e2c1f1c3005c01681ce6402df2dde82a9d1
Dan Pierce <nxw867@motorolasolutions.com>
2019-01-03 10:50:59 -0500

    Finished testing read and write on full buffers

--- src/RAMBufferDriver_test.cpp
+++ src/RAMBufferDriver_test.cpp
@@ -71,3 +71,54 @@ TEST(RAMBufferDriverTests, TwoStringTest)
 	delete [] buf;
 
 }
+
+const string fullbufferteststring = "ABCDEFGHIJKLMNOPQRSTU";  // 21 characters
+
+TEST(RAMBufferDriverTests, FullBufferTest)
+{
+	const int nRamBufferSize = 1024;
+	int fd;
+	char *buf = new char[fullbufferteststring.length()];
+
+	RAMBufferDriver *rb = new RAMBufferDriver(nRamBufferSize);
+	fd = rb->open("MyRamBuffer", O_RDWR | O_CREAT);
+
+	unsigned int loops = nRamBufferSize / fullbufferteststring.length();
+	unsigned int numbytes = 0;
+	for(unsigned int i=0; i < loops; i++)
+	{
+		numbytes = rb->write(fd, (void *)fullbufferteststring.c_str(), fullbufferteststring.length());
+		CHECK_TRUE(numbytes == fullbufferteststring.length());
+	}
+
+	// write the last partial buffer.
+	numbytes = rb->write(fd, (void *)fullbufferteststring.c_str(), fullbufferteststring.length());
+	CHECK_TRUE(numbytes == nRamBufferSize % fullbufferteststring.length());
+
+	// finally make sure a write after this gets a 0
+	numbytes = rb->write(fd, (void *)fullbufferteststring.c_str(), fullbufferteststring.length());
+	CHECK_TRUE(numbytes == 0);
+
+	// Now let's read it all back
+	for(unsigned int i=0; i < loops; i++)
+	{
+		memset(buf, '.', fullbufferteststring.length());
+		numbytes = rb->read(fd, buf, fullbufferteststring.length());
+		CHECK_TRUE(numbytes == fullbufferteststring.length());
+		CHECK_TRUE(strncmp((const char *)buf, (const char *)fullbufferteststring.c_str(), fullbufferteststring.length()) == 0);
+	}
+	// read the last partial buffer
+	memset(buf, '.', fullbufferteststring.length());
+	numbytes = rb->read(fd, buf, fullbufferteststring.length());
+	CHECK_TRUE(numbytes == nRamBufferSize % fullbufferteststring.length());
+	CHECK_TRUE(strncmp((const char *)buf, (const char *)fullbufferteststring.c_str(), nRamBufferSize % fullbufferteststring.length()) == 0);
+
+	// finally make sure a write after this gets a 0
+	numbytes = rb->read(fd, buf, fullbufferteststring.length());
+	CHECK_TRUE(numbytes == 0);
+
+	rb->close(fd);
+	delete rb;
+	delete [] buf;
+
+}
